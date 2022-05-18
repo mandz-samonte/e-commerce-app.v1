7e918858731e72d45bafd60bca7307c6452f0f13
DESKTOP-BF3VM9G\John Mandy Samonte <johnmandysamonte@gmail.com>
2022-05-18 18:34:18 +0800

    [UPDATE] react setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        axios.get("https://fakestoreapi.com/products/1").then(({ data }) => {
+            console.log(data);
+            setProduct(data);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={product.image} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
